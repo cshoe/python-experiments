b82efe78ab6e2428aa8a84426e29f4c36b09f5cf
Chris Schomaker <schomaker.c@gmail.com>
2013-02-04 23:56:03 -0500

    Change system volume with applescript

---
+++ applescript/README.rst
@@ -0,0 +1,7 @@
+AppleScript
+-----------
+
+running_applescript.py
+++++++++++++++++++++++
+
+Trying to figure out the best way to run AppleScript with Python.
---
+++ applescript/running_applescript.py
@@ -0,0 +1,18 @@
+import os
+
+
+def main():
+    cmd = """osascript<<END
+    set vol to output volume of (get volume settings)
+    set vol_interval to 5
+    if vol > (100 - vol_interval) then
+	set volume output volume 100
+    else
+	set volume output volume (vol + 5)
+    end if
+    END"""
+    os.system(cmd)
+
+
+if __name__ == '__main__':
+    main()
